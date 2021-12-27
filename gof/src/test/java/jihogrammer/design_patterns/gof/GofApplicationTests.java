20ae9c330bb5c7e9f9bf3d137a838e50085218ca
Jihogrammer <jiho@kakao.com>
2021-12-27 15:56:29 +0900

    init: add a java project

---
+++ gof/src/test/java/jihogrammer/design_patterns/gof/GofApplicationTests.java
@@ -0,0 +1,13 @@
+package jihogrammer.design_patterns.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
